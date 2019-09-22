ccceb2d9dad8726d70b6f4efb271f06f35844004
Jim <jimhlad@gmail.com>
2019-09-22 11:22:25 -0400

    Add default config file

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Custom cast type aliases
+    |--------------------------------------------------------------------------
+    |
+    | The custom cast type aliases for the application. These are defined by 
+    | specifying the alias as the array key, and the fully qualified cast class 
+    | name as the value. For example:
+	|
+    | 'prefix_name' => PrefixNameCast::class,
+    |
+    */
+];
